985075836089485e24191efb195e77cb2ef0f115
shiv.kr.thakur <shivkrthakur@users.noreply.github.com>
2017-11-22 00:33:43 -0800

    Added MSSQL code for the SQL-EX.RU SELECT (Learning Stage) Exercise 19.

---
+++ SQLExercises/SELECT (LEARNING STAGE)/Exercise19.sql
@@ -0,0 +1,22 @@
+/*	author:@shivkrthakur  */
+/*
+Short database description "Computer firm":
+
+The database scheme consists of four tables:
+Product(maker, model, type)
+PC(code, model, speed, ram, hd, cd, price)
+Laptop(code, model, speed, ram, hd, screen, price)
+Printer(code, model, color, type, price)
+
+The Product table contains data on the maker, model number, and type of product ('PC', 'Laptop', or 'Printer'). It is assumed that model numbers in the Product table are unique for all makers and product types. Each personal computer in the PC table is unambiguously identified by a unique code, and is additionally characterized by its model (foreign key referring to the Product table), processor speed (in MHz) – speed field, RAM capacity (in Mb) - ram, hard disk drive capacity (in Gb) – hd, CD-ROM speed (e.g, '4x') - cd, and its price. The Laptop table is similar to the PC table, except that instead of the CD-ROM speed, it contains the screen size (in inches) – screen. For each printer model in the Printer table, its output type (‘y’ for color and ‘n’ for monochrome) – color field, printing technology ('Laser', 'Jet', or 'Matrix') – type, and price are specified.
+
+Exercise: 19 (Serge I: 2003-02-13)
+For each maker having models in the Laptop table, find out the average screen size of the laptops he produces. 
+Result set: maker, average screen size.
+*/
+
+SELECT product.maker, avg(LAPTOP.screen) as [Average Screen Size]
+FROM LAPTOP 
+INNER JOIN PRODUCT on laptop.model = product.model
+group by product.maker
+
